1c887d21c9ac689affb2544cba43969deb9566fc
Kyle Michael Deguzman <kylemichaeldeguzman@kyles-mbp.home>
2021-03-24 09:15:09 -0700

    setup app routes

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 
 import {reducer as form} from 'redux-form';
 
-const rootReducer = combineReducers({ // ability to type within search bar
+const rootReducer = combineReducers({ // ability to type within search bar and will display query url
   form
 });
 
