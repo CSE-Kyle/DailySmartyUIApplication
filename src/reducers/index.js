a2f5d707d445c64d43116587ff02a122a2e2f3f7
Kyle Michael Deguzman <kylemichaeldeguzman@kyles-mbp.home>
2021-03-24 12:16:51 -0700

    setup redux devtools and fixed reducer for set_recent_posts

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import {reducer as form} from 'redux-form';
+import posts from './postReducer';
 
 const rootReducer = combineReducers({ // ability to type within search bar and will display query url
-  form
+  form,
+  posts
 });
 
 export default rootReducer;
