98303cac2b9b21496254a7af6d30653ea16e8c45
Kyle Michael Deguzman <kylemichaeldeguzman@kyles-mbp.home>
2021-03-28 08:26:21 -0700

    setup redux devtools and fixed reducer for set_recent_posts

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,9 +1,10 @@
 import {
-    SET_RECENT_POSTS
+    SET_RECENT_POSTS,
+    SET_RESULTS_POSTS
 } from '../actions/types'; // getting access to actions
 
 const INIT_STATE = {
-    posts: [], // fetching for posts
+    resultsPosts: [], // fetching for posts
     recentPosts: []
 }
 
@@ -11,8 +12,16 @@ export default function(state = INIT_STATE, action) {
     switch (action.type) { // determining which action you're hitting
         case SET_RECENT_POSTS:
             const recentPosts = action.payload; // connected to payload within index.js file (actions); being dispatched
-            return {...state, recentPosts};
-    
+            return {
+                ...state, 
+                recentPosts
+            };
+        case SET_RESULTS_POSTS:
+            const resultsPosts = action.payload;
+            return {
+                ...state,
+                resultsPosts
+            };
         default:
             return state;
     }
