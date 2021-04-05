280cefbcca2611a11d2a93e846a86e6d41c61400
Kyle Michael Deguzman <kylemichaeldeguzman@kyles-mbp.home>
2021-04-04 17:45:16 -0700

    finished JS substrings for post links

--- src/components/post.js
+++ src/components/post.js
@@ -17,6 +17,27 @@ class Post extends Component {
     return topics; // displays associated topics
   }
 
+  getNameForPostLink(str) {
+    var n = str.lastIndexOf('/');
+    var link = str.substring(n + 1, str.length);
+
+    if ((n + 1) == str.length) {
+      link = str.slice(0, n);
+      n = link.lastIndexOf('/');
+      link = str.substring(n + 1, str.length - 1);
+    }
+
+    if (link.includes('.html')) {
+      link = link.substring(0, link.length - 5);
+    }
+
+    if (link.includes(".htm")) {
+      link = link.substring(0, link.length - 4);
+    }
+
+    return link;
+  }
+
   renderLinks() {
     let links = this.props.post_links.map((post_link, index) => {
       return (
@@ -26,7 +47,7 @@ class Post extends Component {
           </div>
 
           <div className="post-link__link">
-            <a href={post_link.link_url}>Useful Link #{index + 1}</a> {/* link_url referred within DailySmarty API link in postman (https://api.dailysmarty.com/search?q=results) */}
+            <a href={post_link.link_url}>{this.getNameForPostLink(post_link.link_url)}</a> {/* link_url referred within DailySmarty API link in postman (https://api.dailysmarty.com/search?q=results) */}
             {/* {index + 1} - will start at actual value instead of index value (starts at 1 instead of 0) */}
           </div>
         </div>
